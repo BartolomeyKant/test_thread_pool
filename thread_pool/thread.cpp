c1e480fb98be9e77c92a904ecf4e055cfd41ce76
Notevill <Notevill@gmail.com>
2021-09-04 23:04:25 +0500

    fix using actions and complete thread syncronization with mutexes

--- thread_pool/thread.cpp
+++ thread_pool/thread.cpp
@@ -8,11 +8,13 @@ namespace thread_pool
 	{
 		_need_work.store(false);
 		_cv_action.notify_one();
-		if (!_is_busy.load() && _thr.joinable()) {
+		if (!_is_busy.load() && _thr.joinable())
+		{
 			// дожидаемся безопасного завершения потока
 			_thr.join();
 		}
-		else {
+		else
+		{
 			// иначе даже не пытаемся дождаться, вызываем terminate
 			_thr.~thread();
 		}
@@ -24,22 +26,34 @@ namespace thread_pool
 		_is_started = true;
 	}
 
-	void Thread::run_action(unique_ptr<ActionBase> &&action)
+	void Thread::run_action(const shared_ptr<Action> &action)
 	{
-		_action = forward<unique_ptr<ActionBase>>(action);
+		unique_lock<mutex> lk(_action_mutex);
+		_is_busy.store(true);
+		_action = action;
 		_cv_action.notify_one();
 	}
 
 	void Thread::loop()
 	{
 		unique_lock<mutex> lk(_cv_mutex);
+
+		// лочит мутекс и возвращает true, так что это можно использовать в условии while
+		auto lock_if = [&mx = this->_action_mutex](bool val)
+		{
+			mx.lock();
+			if (!val) {
+				mx.unlock();
+			}
+			return val;
+		};
 		do
 		{
-			while (_action)
+			while (lock_if(static_cast<bool>(_action)))
 			{
-				_is_busy.store(true);
 				_action->run();
 				_action.reset();
+				_action_mutex.unlock();
 				if (_cb_end_action)
 				{
 					// при вызове _cb_end_action может обновится _action
@@ -47,7 +61,6 @@ namespace thread_pool
 				}
 			}
 			_is_busy.store(false);
-
 			_cv_action.wait(lk);
 		} while (_need_work.load());
 	}
--- thread_pool/thread.hh
+++ thread_pool/thread.hh
@@ -30,10 +30,10 @@ namespace thread_pool
 
 		/**
 		 * Задать новое действие для потока и запустить выполнение
-		 * 
+		 *
 		 * @param action действие, которое нужно запустить в этом потоке
 		*/
-		void run_action(std::unique_ptr<ActionBase> &&action);
+		void run_action(const std::shared_ptr<Action> &action);
 
 		/**
 		 * Проверка, если поток занят
@@ -53,12 +53,15 @@ namespace thread_pool
 		std::atomic_bool _need_work = true;
 
 		std::thread _thr;
+
+		std::mutex _action_mutex;
+
 		std::mutex _cv_mutex;
 		std::condition_variable _cv_action;
 
-		std::unique_ptr<ActionBase> _action;
+		std::shared_ptr<Action> _action;
 
-		// для упрощения доступа к _cb_end_action добавляем friend class 
+		// для упрощения доступа к _cb_end_action добавляем friend class
 		friend class ThreadPool;
 		// callback для уведомления об окончании выполнения действия
 		std::function<void(Thread &)>
