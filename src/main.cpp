023f26c3620f30db5ccbe1382929616b4ef99d42
Notevill <Notevill@gmail.com>
2021-09-04 23:05:41 +0500

    work on some examples

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,13 @@ void do_sleep(int time, float d) {
 	}
 }
 
+void do_work(int &res, int) {
+	res = 10;
+}
+
 struct A {
-	void foo(string s) {
+	void foo(string& s) {
+		s = "new value";
 		cout << "call to foo " << s << endl;
 	}
 };
@@ -36,14 +41,31 @@ int main(int argc, char **argv)
 	function<void (int, float)> f = &do_sleep;
 
 	A a;
+	string ss = "asdasd";
+	auto res_wait1 = _pool.run_action(&A::foo, a, ss);
 
-	_pool.run_action([](int i, float f)
+	auto res_wait2 = _pool.run_action(&do_sleep, 3, 4.12f);
+	auto res_wait3 = _pool.run_action<void>([](int i, float f)
 					 { cout << "run lambda " << i << " " << 4.3 << endl; },
 					 5, 4.3f);
-	_pool.run_action(&do_sleep, 8, 4.12f);
-	_pool.run_action(&A::foo, a, string("asdasd"));
 
-	this_thread::sleep_for(chrono::seconds(15));
+	auto res_wait4 = _pool.run_action<int>([](int a)->int {return a+1;}, 3);
+
+	int change_value =  0;
+	auto res_wait5 = _pool.run_action<void>([&change_value](){change_value++;});
+
+	res_wait4.wait();
+	cout << "wait4 res: " << res_wait4.res() << endl;
+
+	res_wait5.wait();
+	cout << "wait5 res: " << change_value << endl;
+
+	res_wait1.wait();
+	cout << "1 completed" << endl;
+	res_wait2.wait();
+	cout << "2 completed" << endl;
+	res_wait3.wait();
+	cout << "3 completed" << endl;
 
 	return 0;
 }
